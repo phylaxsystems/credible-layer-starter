b001c886c5ad019a19605a5ac980c05867c1ea52
Frederik <luehrs.fred@gmail.com>
2026-03-18 14:42:43 +0100

    fix: allow using account, no need to expose env variables anymore (#19)
    * fix: allow using account, no need to expose env variables anymore
    
    * fix: Change naming of script to match filename

--- script/DeployPhyLock.s.sol
+++ script/DeployPhyLock.s.sol
@@ -1,21 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import "forge-std/Script.sol";
+import {console} from "forge-std/Script.sol";
+import {DeployBase} from "./DeployBase.s.sol";
 import "../src/PhyLock.sol";
 
-contract DeployScript is Script {
-    function run() public {
-        // Load private key from environment
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
-
-        // Deploy PhyLock contract
-        // Note: The PhylaxToken will be automatically deployed by the PhyLock constructor
+contract DeployPhyLock is DeployBase {
+    function run() public broadcast {
         PhyLock phyLock = new PhyLock();
         console.log("PhyLock deployed at:", address(phyLock));
         console.log("PhylaxToken deployed at:", address(phyLock.phylaxToken()));
-
-        vm.stopBroadcast();
     }
 }
